7ef8227ef1c8642dec34ba9523234998306239a9
Diogo "Coelho" Gonçalves <coelho.diogogoncalves@gmail.com>
2025-05-11 16:27:41 +0100

    Fixed everything

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -280,21 +280,21 @@ $( document ).ready(function() {
 });
 
 function playGame(playerChoice) {
-    const choices = ['rock', 'paper', 'scissors'];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    let result = '';
+  const choices = ['rock', 'paper', 'scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = '';
 
-    if (playerChoice === computerChoice) {
-      result = "Empate! 😐";
-    } else if (
-      (playerChoice === 'rock' && computerChoice === 'scissors') ||
-      (playerChoice === 'paper' && computerChoice === 'rock') ||
-      (playerChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-      result = `Ganhaste! 🏆 (${playerChoice} vence ${computerChoice})`;
-    } else {
-      result = `Perdeste! 😢 (${computerChoice} vence ${playerChoice})`;
-    }
-
-    document.getElementById('game-result').innerText = result;
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result = "You win!";
+  } else {
+    result = "You lose!";
   }
+
+  document.getElementById('game-result').innerText = `I chose ${computerChoice}. ${result}`;
+}
